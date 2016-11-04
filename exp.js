66a0dbd062348bce35a09c999d781a9f20e167ef
Daniel Rivas <rivasdaniel1992@gmail.com>
2016-11-04 16:19:16 -0400

    switched to new serverPsych

--- exp.js
+++ exp.js
@@ -1,17 +1,13 @@
 
-
-
-
-
-     function runExperiment(){
-    	 djPsych.request(function(params){
-    		 jsPsych.init({
-    			 timeline : params.timeline,
-    			 on_finish : function(data){
-    				 djPsych.save(data, false);
-    			 }
-    		 });
-    		 
-    	 });   	     	 
-    	 
-     };
+ function runExperiment(){
+	 serverPsych.request(function(params){
+		 jsPsych.init({
+			 timeline : params.timeline,
+			 on_finish : function(data){
+				 serverPsych.save(data, false);
+			 }
+		 });
+		 
+	 });   	     	 
+	 
+ };
